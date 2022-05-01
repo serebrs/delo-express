4c7de596f4e12a4d6a54afa41d7ac7664145eb4c
serebrs <serebr.s@gmail.com>
2022-05-01 20:24:25 +0600

    .

--- controllers/docs.js
+++ controllers/docs.js
@@ -1,4 +1,5 @@
 import path from "path";
+import fs from "fs";
 import { validationResult } from "express-validator";
 
 let data = [
@@ -29,7 +30,7 @@ let data = [
     title:
       "The advantage is that the dev machine does not need to have all of the build tools and environments installed",
     person: ["Александров А.А."],
-    file: "file1.pdf",
+    file: "file01.pdf",
   },
   {
     id: 4,
@@ -39,7 +40,7 @@ let data = [
     title:
       "To run our container to support a development workflow, we will do the following",
     person: ["J. Cooper", "Сидоров С.С."],
-    file: "file1.pdf",
+    file: "file02.pdf",
   },
   {
     id: 5,
@@ -48,7 +49,7 @@ let data = [
     date: "2021-12-05",
     title: "Install all dependencies, including the dev dependencies",
     person: ["J. Cooper"],
-    file: "file1.pdf",
+    file: "file03.pdf",
   },
   {
     id: 6,
@@ -58,7 +59,7 @@ let data = [
     title:
       "Regional Paradigm Technician Regional Paradigm Technician Regional Paradigm Technician",
     person: ["J. Cooper", "Васильев В.В."],
-    file: "file1.pdf",
+    file: "file04.pdf",
   },
   {
     id: 7,
@@ -67,7 +68,7 @@ let data = [
     date: "2021-11-16",
     title: "Using bind mounts is very common for local development setups",
     person: ["Иванов И.И."],
-    file: "file1.pdf",
+    file: "file05.pdf",
   },
   {
     id: 8,
@@ -77,7 +78,7 @@ let data = [
     title:
       "The advantage is that the dev machine does not need to have all of the build tools and environments installed",
     person: ["Александров А.А."],
-    file: "file1.pdf",
+    file: "file06.pdf",
   },
   {
     id: 9,
@@ -87,7 +88,7 @@ let data = [
     title:
       "To run our container to support a development workflow, we will do the following",
     person: ["J. Cooper", "Сидоров С.С."],
-    file: "file1.pdf",
+    file: "file07.pdf",
   },
   {
     id: 10,
@@ -96,7 +97,7 @@ let data = [
     date: "2021-12-05",
     title: "Install all dependencies, including the dev dependencies",
     person: ["J. Cooper"],
-    file: "file1.pdf",
+    file: "file08.pdf",
   },
   {
     id: 11,
@@ -106,7 +107,7 @@ let data = [
     title:
       "Regional Paradigm Technician Regional Paradigm Technician Regional Paradigm Technician",
     person: ["J. Cooper", "Васильев В.В."],
-    file: "file1.pdf",
+    file: "file09.pdf",
   },
   {
     id: 12,
@@ -115,7 +116,7 @@ let data = [
     date: "2021-11-16",
     title: "Using bind mounts is very common for local development setups",
     person: ["Иванов И.И."],
-    file: "file1.pdf",
+    file: "file10.pdf",
   },
   {
     id: 13,
@@ -125,7 +126,7 @@ let data = [
     title:
       "The advantage is that the dev machine does not need to have all of the build tools and environments installed",
     person: ["Александров А.А."],
-    file: "file1.pdf",
+    file: "file11.pdf",
   },
   {
     id: 14,
@@ -135,7 +136,7 @@ let data = [
     title:
       "To run our container to support a development workflow, we will do the following",
     person: ["J. Cooper", "Сидоров С.С."],
-    file: "file1.pdf",
+    file: "file12.pdf",
   },
   {
     id: 15,
@@ -144,12 +145,13 @@ let data = [
     date: "2021-12-05",
     title: "Install all dependencies, including the dev dependencies",
     person: ["J. Cooper"],
-    file: "file1.pdf",
+    file: "file13.pdf",
   },
 ];
 
 export const getAllDocs = (req, res) => {
-  res.status(200).json(data);
+  if (data) res.status(200).json(data);
+  else res.status(404).end();
 };
 
 export const addDoc = (req, res) => {
@@ -168,20 +170,37 @@ export const addDoc = (req, res) => {
 
 export const deleteDoc = (req, res) => {
   data = data.filter((r) => r.id !== +req.params.id);
-  res.status(200).json({ message: "Документ успешно удален" });
+  if (data) res.status(200).json({ message: "Документ успешно удален" });
+  else res.status(404).end();
 };
 
 export const changeDoc = (req, res) => {
+  let file;
   const idx = data.findIndex((r) => r.id === +req.params.id);
-  data[idx] = { ...req.body, file: req.file.filename };
-  res.status(200).json({ message: "Документ успешно изменен" });
+  if (~idx) {
+    // != -1
+    if (req.file) file = req.file.filename;
+    else file = data[idx].file;
+    data[idx] = { ...req.body, id: +req.params.id, file };
+    res.status(200).json({ message: "Документ успешно изменен" });
+  } else res.status(404).end();
 };
 
-export const viewDoc = (req, res) => {
+export const downloadDoc = (req, res) => {
   const doc = data.find((r) => r.id === +req.params.id);
-  const docPath = path.resolve("uploads/", doc.file);
-  // TODO send if file exist 
-  if (path.extname(doc.file) === ".pdf") {
-    res.status(200).sendFile(docPath);
-  } else res.status(200).download(docPath);
+  if (doc) {
+    const docPath = path.resolve("uploads/", doc.file);
+    if (fs.existsSync(docPath)) {
+      if (path.extname(doc.file) === ".pdf") {
+        res.status(200).sendFile(docPath);
+      } else res.status(200).download(docPath);
+    } else res.status(404).end();
+  } else res.status(404).end();
+};
+
+export const detailDoc = (req, res) => {
+  const doc = data.find((r) => r.id === +req.params.id);
+  if (doc) {
+    res.status(200).json(doc);
+  } else res.status(404).end();
 };
--- routes/docs.js
+++ routes/docs.js
@@ -2,7 +2,7 @@ import multer from "multer";
 import { Router } from "express";
 import { checkSchema } from "express-validator";
 import { docsValidator } from "../validators/docs.js";
-import { getAllDocs, addDoc, deleteDoc, viewDoc, changeDoc } from "../controllers/docs.js";
+import { getAllDocs, addDoc, deleteDoc, detailDoc, downloadDoc, changeDoc } from "../controllers/docs.js";
 
 const storage = multer.diskStorage({
   destination: "./uploads",
@@ -18,7 +18,8 @@ const upload = multer({ storage: storage });
 const router = Router();
 
 router.get("/api/docs", getAllDocs);
-router.get("/api/docs/:id", viewDoc);
+router.get("/api/docs/:id", detailDoc);
+router.get("/api/docs/:id/download", downloadDoc);
 router.delete("/api/docs/:id", deleteDoc);
 router.post(
   "/api/docs",
